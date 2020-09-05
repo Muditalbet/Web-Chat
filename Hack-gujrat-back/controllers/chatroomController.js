e34ef009e737c19bd433be8f392f42cc64e7d37c
Muditalbet <muditalbet@gmail.com>
2020-09-05 16:55:19 +0530

    New Files

---
+++ Hack-gujrat-back/controllers/chatroomController.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Chatroom = mongoose.model('Chatroom');
+
+exports.createChatroom = async (req, res)=>{
+    const {name} = req.body;
+    const nameregex = /^[A-Za-z\S]+$/;
+    if(!nameregex.test(name)) throw "Chatroom name can contaon only alphabets!";
+    const chatroomExists = await Chatroom.findOne({name});
+    if(chatroomExists) throw "Chatroom already created";
+    else{
+        const chatroom = new Chatroom({
+            name,
+        })
+        await chatroom.save();
+        res.json({
+            message:"Chatroom created!!"
+        })
+    }
+}
+
+exports.getAllChatrooms = async (req, res) => {
+    const chatrooms = await Chatroom.find({});
+    return res.json(chatrooms);
+};
---
+++ Hack-gujrat-front/src/App.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { BrowserRouter, Switch, Route, useHistory, Link } from "react-router-dom";
+import LoginPage from "./pages/Loginpage";
+import RegisterPage from "./pages/Registerpage";
+import DashboardPage from "./pages/Dashboardpage";
+import IndexPage from "./pages/Indexpage";
+import Chatroompage from "./pages/Chatroompage";
+import io from "socket.io-client";
+import makeToast from "./Toaster";
+
+function App() {
+  const history = useHistory()
+  const [socket, setSocket] = React.useState(null);
+  const setupSocket = () => {
+    const token = localStorage.getItem("CC_Token");
+    if (token && !socket) {
+      const newSocket = io("http://localhost:8000", {
+        query: {
+          token: localStorage.getItem("CC_Token"),
+        },
+      });
+      newSocket.on("disconnect", () => {
+        setSocket(null);
+        setTimeout(setupSocket, 3000);
+        makeToast("error", "Socket Disconnected!");
+      });
+
+      newSocket.on("connect", () => {
+        makeToast("success", "Socket Connected!");
+      });
+
+      setSocket(newSocket);
+    }
+  };
+  React.useEffect(() => {
+    setupSocket();
+    //eslint-disable-next-line
+  }, []);
+  return(
+    <BrowserRouter>
+        <Link to="/login">
+          <button
+            onClick={()=>{
+              localStorage.clear()
+            }}
+          >logout</button>
+        </Link>
+      <Switch>
+        <Route path="/" component={IndexPage} exact />
+        <Route path="/login" render={()=><LoginPage setupSocket={setupSocket} />} exact />
+        <Route path="/register" component={RegisterPage} exact />
+        <Route path="/dashboard" render={()=><DashboardPage socket={socket} />} exact />
+        <Route path="/chatroom/:id" render={()=><Chatroompage socket={socket} />} exact />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
